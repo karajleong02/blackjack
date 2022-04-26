71c5d0f6574936c73c6e831f3fa5b4e09f213743
karaleong <kleong8@gatech.edu>
2022-04-26 15:22:27 -0400

    card logic working

--- public/tasks.js
+++ public/tasks.js
@@ -43,25 +43,35 @@ function newGame() {
 
 
 function getCard(ind, isPlayer) { //for person 0 is player, 1 is dealer
-    
+    console.log("inside getCard");
     let xhr = new XMLHttpRequest;
     xhr.addEventListener ("load", function() {
         card = JSON.stringify(xhr.response.cards[0].value);
         card = card.split("\"")[1];
-        if(!isNaN(parseInt(card))){
-            if (isPlayer == 0) {
-                playerCards[ind] = parseInt(card);
-                playerTotal += playerCards[ind];
-                document.getElementById("player_score").innerHTML = "Your Total: " + playerTotal;
-                 //document.getElementById("p" + pInd).src = ""; //USE API TO GET THE PICTURE, DOES THE CONCATENATION WORK
+        let val = 0;
+        console.log("before if statement")
+        if(!isNaN(parseInt(card))){ 
+            val = parseInt(card)
+        } else {
+            if (card === "ACE") {
+                val = 11;
             } else {
-                console.log("hello");
-                dealerCards[ind] = parseInt(card);
+                val = 10;
+            }
+        }
+        if (isPlayer == 0) {
+            playerCards[ind] = val;
+            playerTotal += playerCards[ind];
+            document.getElementById("player_score").innerHTML = "Your Total: " + playerTotal;
+             //document.getElementById("p" + pInd).src = ""; //USE API TO GET THE PICTURE, DOES THE CONCATENATION WORK
+        } else if (isPlayer == 1){
+            if (dealerTotal < 17) {
+                dealerCards[ind] = val;
                 dealerTotal += dealerCards[ind];
                 document.getElementById("dealer_score").innerHTML = "Dealer Total: " + dealerTotal;
-                document.getElementById("d" + dInd).src = "";
+                //document.getElementById("d" + dInd).src = "";
             }
-        }   
+        }  
     })
     xhr.responseType = "json";
     xhr.open("GET", "https://deckofcardsapi.com/api/deck/" + deckID + "/draw/?count=1");
@@ -86,15 +96,9 @@ function playerDraw() {
 
 function dealerDraw() {
     //USE SOME SORT OF TIMER
-    console.log("gets here");
-    while(dealerTotal < 17 && dealerCards.length < 7) {
-        
-        let dInd = dealerCards.length;
-        // dealerCards[dInd] = 1;
-        // dealerTotal += dealerCards[dInd];
-        getCard(dInd, 1);
-        
-        
+   
+    for(i = 0; i < 7; i++) {
+        getCard(i, 1);
     }
     checkWin();
 }
