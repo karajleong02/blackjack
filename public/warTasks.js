4e42a62cd1d5bcebd8eaf65c74c2f6ea77d6d8fd
karaleong <kleong8@gatech.edu>
2022-04-27 16:39:47 -0400

    war works

---
+++ public/warTasks.js
@@ -0,0 +1,110 @@
+var playerCard = "0";
+var dealerCard = "0";
+var wins = 0;
+var deck;
+let deckID = "wx7rcacs4dsd";
+
+
+
+// add event listener for button
+window.addEventListener("DOMContentLoaded", domLoaded);
+
+function domLoaded() {
+    newGame();
+    // document.getElementById("player_score").innerHTML = "You: " + playerCard;
+    //document.getElementById("dealer_score").innerHTML = "Computer: " + dealerCard;
+
+    const drawBtn = document.getElementById("draw_button");
+    drawBtn.addEventListener("click", getCards);
+
+    const restartBtn = document.getElementById("restart_button");
+    restartBtn.addEventListener("click", restartGame);
+}
+
+function newGame() {
+    
+    let xhr = new XMLHttpRequest;
+    var cardid = document.getElementById("cardid");
+    xhr.addEventListener ("load", function() {
+        deckID = xhr.response.deck_id;
+        cardid.innerHTML = deckID;
+        deckID = (cardid.innerHTML);
+    })
+    xhr.responseType = "json";
+    xhr.open("GET", "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=6");
+    xhr.send();
+}
+
+
+function getCard() { //for person 0 is player, 1 is dealer
+    let xhr = new XMLHttpRequest;
+    xhr.addEventListener ("load", function() {
+        card1 = JSON.stringify(xhr.response.cards[0].value).split("\"")[1];
+        card2 = JSON.stringify(xhr.response.cards[1].value).split("\"")[1];
+        img1 = JSON.stringify(xhr.response.cards[0].image);
+        img2 = JSON.stringify(xhr.response.cards[1].image);
+       
+        playerCard = card1;
+        document.getElementById("p1").src = img1.split("\"")[1]; 
+        document.getElementById("p1").style.display = "initial";
+        document.getElementById("player_score").innerHTML = "Your Card: " + playerCard;
+    
+        dealerCard = card2;
+        document.getElementById("c1" ).src = img2.split("\"")[1];
+        document.getElementById("c1").style.display = "initial";
+        document.getElementById("computer_score").innerHTML = "Computer's Card: " + dealerCard;
+
+        checkWin();
+        
+    })
+    xhr.responseType = "json";
+    xhr.open("GET", "https://deckofcardsapi.com/api/deck/" + deckID + "/draw/?count=2");
+    xhr.send();
+    
+    
+}
+
+function getCards() {
+    getCard();
+}
+
+function restartGame() {
+    playerCard = "0";
+    dealerCard = "0";
+    wins = 0;
+    document.getElementById("p1").src = " "; 
+    document.getElementById("p1").style.display = "none";
+    document.getElementById("c1").src = " "; 
+    document.getElementById("c1").style.display = "none";
+    document.getElementById("numWins").innerHTML = "Current Win Streak: 0";
+    document.getElementById("player_score").innerHTML = "Your Card: " + playerCard;
+    document.getElementById("computer_score").innerHTML = "Computer's Card: " + dealerCard;
+}
+
+function checkWin() {
+   let pc = sortVal(playerCard);
+   let dc = sortVal(dealerCard);
+   console.log(dc + " " + pc);
+   if (pc > dc) {
+       console.log("win");
+       wins++;
+   } else if (pc < dc) {
+       console.log("lose");
+       wins = 0;
+   }
+   document.getElementById("numWins").innerHTML = "Current Win Streak: " + wins;
+}
+
+function sortVal(card) {
+    if (card === "ACE") {
+        return 14;
+    } else if (card === "KING") {
+        return 13;
+    } else if (card === "QUEEN") {
+        return 12;
+    } else if (card === "JACK") {
+        return 11;
+    } else {
+        return parseInt(card);
+    }
+}
